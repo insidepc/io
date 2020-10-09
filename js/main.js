961fe8e3893196d5e3be7ca1927a8be0838be3b7
Hamza hazem <clash.at.112001@gmail.com>
2020-10-09 19:38:32 +0300

    update

--- js/main.js
+++ js/main.js
@@ -57,10 +57,10 @@ $(function() {
 var mybutton = document.getElementById("scroll-up");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction(20)};
+window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
@@ -72,6 +72,8 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
 
 $(".toggle-icons").click(function() {
   $(".toggle-icons").toggleClass("rot");
